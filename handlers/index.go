8a4c1bf61695d4903999841a0e505c28a43f1819
Onsi Fakhouri <onsijoe@gmail.com>
2014-11-04 12:33:38 -0800

    get the index from CF_INSTANCE_INDEX

--- handlers/index.go
+++ handlers/index.go
@@ -1,22 +1,14 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
-
-	"github.com/onsi/grace/vcap_application_parser"
+	"os"
 )
 
 type InstanceIndex struct {
 }
 
 func (p *InstanceIndex) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	index, err := vcap_application_parser.GetIndex()
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-
-	w.Write([]byte(fmt.Sprintf("%d", index)))
+	index := os.Getenv("CF_INSTANCE_INDEX")
+	w.Write([]byte(index))
 }
